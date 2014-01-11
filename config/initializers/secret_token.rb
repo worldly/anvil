8faa022d5a2068842314ec1eb986269615a87931
Jeshua Borges <jesh@jesh.me>
2014-01-11 15:33:01 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anvil::Application.config.secret_key_base = 'f595d06e45f35aef5eedf0d06eb049ab0e2f1cbfb53759fa265843ceadbd09266a58301cdfe8cd9d7cf2bc3d4f0215bc72a91b4cd19424a6d02a3423fc2c4c8f'
